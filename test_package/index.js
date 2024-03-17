c4e94085dd61c66ba8b0321db106aef091783d10
i.kłusek <J6ZkqwVUn>
2024-03-17 21:48:09 +0100

    Show modal triggerd from package

--- test_package/index.js
+++ test_package/index.js
@@ -4,3 +4,11 @@ const testPackage = require('test-npm-package-test-string');
 console.log(testPackage.sayHello({ firstName: 'igor', lastName: 'klusek' }));
 console.log(testPackage.testString('text'));
 console.log(testPackage.add(2, 4));
+
+document.addEventListener('DOMContentLoaded', () => {
+  const modalButton = document.querySelector('.modal-button');
+  modalButton.addEventListener('click', () => {
+    // Call the triggerModal function with the specified title and description
+    testPackage.triggerModal('elo', 'description text');
+  });
+});
